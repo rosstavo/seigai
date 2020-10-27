963dd63dc86cf0db0cdfb5289a2e5bb8ab3f6fd6
Ross <ross@rosss-mbp.lan>
2020-10-27 14:56:39 +0000

    Added parallax

--- js/source/main.js
+++ js/source/main.js
@@ -1,9 +1,21 @@
-/**
- * File main.js.
- */
+( function($) {
 
-( function( $ ) {
+    $window = $(window);
 
-    wheelzoom( $('.map'), { maxZoom: 3 } );
+    $('*[data-type="parallax"]').each(function(){
 
-} )( jQuery );
+        var $bgobj = $(this);
+
+        $(window).on( 'scroll resize', $.throttle(100, function() {
+
+            var yPos = ($window.scrollTop() + $window.height()) / $(document).height() * 100;
+            var coords = '50% '+ yPos + '%';
+
+            $bgobj.css({ backgroundPosition: coords });
+
+            console.log(coords);
+
+        } ) );
+    });
+
+} )(jQuery);
--- js/source/throttle-debounce.js
+++ js/source/throttle-debounce.js
@@ -250,24 +250,3 @@
   };
   
 })(this);
-( function($) {
-
-    $window = $(window);
-
-    $('*[data-type="parallax"]').each(function(){
-
-        var $bgobj = $(this);
-
-        $(window).on( 'scroll resize', $.throttle(100, function() {
-
-            var yPos = ($window.scrollTop() + $window.height()) / $(document).height() * 100;
-            var coords = '50% '+ yPos + '%';
-
-            $bgobj.css({ backgroundPosition: coords });
-
-            console.log(coords);
-
-        } ) );
-    });
-
-} )(jQuery);
